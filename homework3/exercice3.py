8b3ab8d053970424331194c921c32a2eabd08d12
vasyl bazhan <116294488+VasylBazhan@users.noreply.github.com>
2022-10-29 16:50:07 +0300

    Add files via upload

---
+++ homework3/exercice3.py
@@ -0,0 +1,10 @@
+password = "899"
+if password == "899":
+    print ("password is true. welcome")
+else:
+    print ("false password. try again")
+password = "899"
+if password == "894":
+    print ("password is true. welcome")
+else:
+    print ("false password. try again")
---
+++ homework3/exercice4.py
@@ -0,0 +1,14 @@
+x1 = 2898
+a1 = x1 % 4
+b1 = x1 % 100
+c1 = x1 % 400
+if x1 < 1900:
+    print ('the year does not meet the condition')
+elif a1 <= 0:
+    print('is leep yea')
+elif c1 <= 0:
+    print('is leep yea')
+else:
+    b1 >= 0
+    print ('is not leep yea')
+print (x1)
